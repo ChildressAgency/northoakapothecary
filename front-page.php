e284ecf836f0debe019a35a92b330d51d43fb87e
halfg33k <johnsonam17@gmail.com>
2018-10-23 17:24:07 -0400

    flexible content

--- front-page.php
+++ front-page.php
@@ -5,7 +5,7 @@
             <div class="carousel-inner">
                 <?php $i = 0; while( have_rows( 'carousel' ) ): the_row(); ?>
                     <div class="carousel-item <?php if( $i == 0 ) echo 'active'; ?>" style="background-image: url('<?php the_sub_field( 'image' ); ?>');">
-                        <div class="dark-overlay"></div>
+                        <div class="overlay overlay--dark"></div>
                         <div class="carousel-caption">
                             <h2 class="carousel__heading"><?php the_sub_field( 'heading' ); ?></h2>
                             <p class="carousel__text"><?php the_sub_field( 'text' ); ?></p>
@@ -18,34 +18,10 @@
                         </div>
                     </div>
                 <?php $i++; endwhile; ?>
-                <!-- <div class="carousel-item" style="background-image: url('http://dev.childressagency.com/northoak/wp-content/uploads/2018/10/carousel-1.png');">
-                    <div class="dark-overlay"></div>
-                    <div class="carousel-caption">
-                        <h2 class="carousel__heading">LEMON HONEY 2</h2>
-                        <p class="carousel__text">Shop our new line of products utilizing our new combination of sustainably sources organic honey from Virginia combined with local organic lemons from South Side Farms. Products ranges from handsoap bars to facemasks. Get stocked up for summer!</p>
-                        <a href="#" class="btn btn-primary">Shop Now</a>
-                        <ol class="carousel-indicators">
-                            <li data-target="#home-carousel" data-slide-to="0"></li>
-                            <li data-target="#home-carousel" data-slide-to="1" class="active"></li>
-                            <li data-target="#home-carousel" data-slide-to="2"></li>
-                        </ol>
-                    </div>
-                </div>
-                <div class="carousel-item" style="background-image: url('http://dev.childressagency.com/northoak/wp-content/uploads/2018/10/carousel-1.png');">
-                    <div class="dark-overlay"></div>
-                    <div class="carousel-caption">
-                        <h2 class="carousel__heading">LEMON HONEY 3</h2>
-                        <p class="carousel__text">Shop our new line of products utilizing our new combination of sustainably sources organic honey from Virginia combined with local organic lemons from South Side Farms. Products ranges from handsoap bars to facemasks. Get stocked up for summer!</p>
-                        <a href="#" class="btn btn-primary">Shop Now</a>
-                        <ol class="carousel-indicators">
-                            <li data-target="#home-carousel" data-slide-to="0"></li>
-                            <li data-target="#home-carousel" data-slide-to="1"></li>
-                            <li data-target="#home-carousel" data-slide-to="2" class="active"></li>
-                        </ol>
-                    </div>
-                </div> -->
             </div>
         </div>
     <?php endif; ?>
+
+    <?php get_template_part( 'tp-flexible-content' ); ?>
 
 <?php get_footer(); ?>
