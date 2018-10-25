73dd278592e5432c69cbd62e4f42f9ab556fc0d5
halfg33k <johnsonam17@gmail.com>
2018-10-25 10:05:17 -0400

    added flexible content

--- page.php
+++ page.php
@@ -1,14 +1,7 @@
 <?php get_header(); ?>
-  <main id="main">
-    <section class="content">
-      <div class="container">
-        <?php if(have_posts()): while(have_posts()): the_post(); ?>
-          <h1 class="page-title"><?php the_title(); ?></h1>
-          <article>
-            <?php the_content(); ?>
-          </article>
-        <?php endwhile; endif; ?>
-      </div>
-    </section>
-  </main>
+    <main>
+        
+        <?php get_template_part( 'template-parts/tp-flexible-content' ); ?>
+
+    </main>
 <?php get_footer(); ?>
