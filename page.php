692885931435c30714990d38a2ee5e8d20b57ae6
halfg33k <johnsonam17@gmail.com>
2018-10-29 14:02:32 -0400

    more content flexibility

--- page.php
+++ page.php
@@ -1,6 +1,9 @@
 <?php get_header(); ?>
     <main>
-        
+        <?php if( have_posts() ): while( have_posts() ): the_post(); ?>
+            <?php the_content(); ?>
+        <?php endwhile; endif; ?>
+
         <?php get_template_part( 'template-parts/tp-flexible-content' ); ?>
 
     </main>
