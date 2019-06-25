97130d3a25f9e04ea8b6d9f65148c53856c189fe
childressdev <jcampbell@childressagency.com>
2019-06-25 17:28:03 -0400

    added container for cart pages

--- page.php
+++ page.php
@@ -1,10 +1,11 @@
 <?php get_header(); ?>
     <main>
+      <?php if(is_cart()){ echo '<div class="container">'; } ?>
         <?php if( have_posts() ): while( have_posts() ): the_post(); ?>
             <?php the_content(); ?>
         <?php endwhile; endif; ?>
 
         <?php get_template_part( 'template-parts/tp-flexible-content' ); ?>
-
+      <?php if(is_cart()){ echo '</div>'; } ?>
     </main>
 <?php get_footer(); ?>
